5062f44bee318e38ccad7031e47e7dabcddfa365
Kaneki Ou <kaneki.king.1493@gmail.com>
2023-09-14 16:56:55 +0900

    hello world出力

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,5 @@
-import {createRoot} from "react-dom";
+import React from 'react';  // 追加
+import { createRoot } from "react-dom/client";
 import App from "./App";
 
 createRoot(document.querySelector("#content")).render(<App />);
